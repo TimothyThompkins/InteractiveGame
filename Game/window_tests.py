b924b8446776637022b2c8edfc94f219d647224b
Timothy Thompkins <mitchell.thompkins@gmail.com>
2015-04-07 00:00:51 +0900

    Got some window stuff working

--- Game/window_tests.py
+++ Game/window_tests.py
@@ -7,38 +7,56 @@ if architecture == 'arm':
 
 from tkinter import *
 from tkinter import ttk
-#from ttk import Frame, Button, Style
+
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+root = Tk()
+root.title("Feet to Meters")
+
+mainframe = ttk.Frame(root, padding="300 300 300 300")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+#background_image = PhotoImage(file='space_invader_word_wallpaper_by_dave109.gif')
+background_image = PhotoImage(file="Space_Invaders_Wallpaper_by_dave109.gif")
+background_label = ttk.Label(mainframe, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+# background_label.photo=background_image
+
+feet = StringVar()
+meters = StringVar()
+
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+# feet_entry.focus()
+root.bind('<Return>', calculate)
+
+root.mainloop()
 
 
-class Example(Frame):
-
-    def __init__(self, parent):
-        Frame.__init__(self, parent)
-
-        self.parent = parent
-
-        self.initUI()
-
-    def initUI(self):
-
-        self.parent.title("Quit button")
-        self.style = Style()
-        self.style.theme_use("default")
-
-        self.pack(fill=BOTH, expand=1)
-
-        quitButton = Button(self, text="Quit",
-            command=self.quit)
-        quitButton.place(x=50, y=50)
-
-
-def main():
-
-    root = Tk()
-    root.geometry("250x150+300+300")
-    app = Example(root)
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
+# def main():
+#
+#     root = Tk()
+#     root.geometry("250x150+300+300")
+#     app = Example(root)
+#     root.mainloop()
+#
+#
+# if __name__ == '__main__':
+#     main()
