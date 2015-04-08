5b8e2f0130442a49e51f2e019fca5b504fa7fd09
Timothy Mitchell Thompkins <mitchell.thompkins@gmail.com>
2015-04-09 02:04:59 +0900

    Just made some slight changes. mostly research

--- Game/python/config.py
+++ Game/python/config.py
@@ -3,18 +3,17 @@ import platform
 class sys_config(object):
 
     def __init__(self):
-        # self.start_window_width = realpart
-        # self.start_window_height = imagpart
-        #
-        # def __set_stats(self):
+
         architecture = (platform.processor())
 
         if architecture == 'arm':
-            # start_window_width = 512
-            # start_window_height = 320
-            #
+
+            # This window height is the pi resolution
+            self.start_window_width = 480
+            self.start_window_height = 320
+
             # game_x_res = 1024
-            # game_y_res = 1024
+            # game_y_res = 640
 
             self.input_options = ['Tilt Sensor']
 
@@ -22,11 +21,10 @@ class sys_config(object):
             self.start_window_width = 512
             self.start_window_height = 320
 
-            self.game_x_res = 1024
-            self.game_y_res = 1024
+            # self.game_x_res = 1024
+            # self.game_y_res = 640
 
             self.input_options = ['Arrow Keys','Web Cam']
-            #self.input_options = 'test'
 
     def get_start_window_dim(self):
         return (self.start_window_width, self.start_window_height)
