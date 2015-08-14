97c1fa25f6eb93fcb93ea8991612f656495e99db
Timothy Mitchell Thompkins <mitchell.thompkins@gmail.com>
2015-08-14 10:02:23 +0900

    Playing with pyserial on OSX

--- Game/python/pyserial_comm_test.py
+++ Game/python/pyserial_comm_test.py
@@ -3,22 +3,22 @@ import time
 #ser = serial.Serial('/dev/tty.usbmodem1431')  # open first serial port
 ser = serial.Serial(
     port='/dev/tty.usbmodem1431',
-    baudrate=9600,
-    #parity=serial.PARITY_ODD,
-    stopbits=serial.STOPBITS_TWO,
-    bytesize=serial.EIGHTBITS
+    baudrate = 9600,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE,
+    bytesize = serial.EIGHTBITS
 )
 
 #ser.open()
 ser.isOpen()
-
-print ('Enter your commands below.\r\nInsert "exit" to leave the application.')
+print ('Connected.')
 
 #input=1
 while 1 :
     # get keyboard input
     #input = raw_input(">> ")
     # Python 3 users
+    print ('Enter your commands below. Insert "exit" to close the application.')
     data = input()
     #sread(size=1)
 
@@ -30,11 +30,20 @@ while 1 :
         # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
         #ser.write(input + '\r\n')
         ser.write(bytes(data, 'UTF-8'))
-        out = ''
+        buffer = ''
         # let's wait one second before reading output (let's give device time to answer)
-        time.sleep(1)
-        while ser.inWaiting() > 0:
-            out += ser.read(1)
+        # time.sleep(1)
 
-        if out != '':
-            print (">>" + out)
+        rcv_data = 0;
+        while rcv_data == 0:
+            # out += ser.readline()
+
+            # out = ser.readline()
+            buffer = ''
+            while ser.inWaiting() > 0:
+                buffer += ser.readline().decode('UTF-8')
+                print (buffer)
+
+            if buffer != '':
+                rcv_data = 1;
+                print (">>" + buffer)
